db2906f96721bca9c51770662c1163565affcc58
rosewhitt <rosewhitt@icloud.com>
2018-06-06 19:37:27 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -33,5 +33,14 @@ def iterate_through_hash
 end
 
 def iterate_through_keys
-  puts "Hello World!"
+  summer_olympics = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
+  summer_olympics[:Atlanta] = "1996"
+  summer_olympics.each_key do |city|
+    puts "The #{city} summer olympics took place in #{year}."
+  end
 end
